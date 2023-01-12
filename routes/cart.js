558040115bedda242966f32a14039f982d09fc07
Reza Adhitya <rezaadhitya1995@gmail.com>
2023-01-12 23:52:04 +0700

    nambah fitur session, nampilin all products

--- routes/cart.js
+++ routes/cart.js
@@ -1,25 +1,26 @@
 const express = require('express');
 const Controller = require('../controllers/controller');
 const router = express.Router();
+const { isLoggedIn, isCustomer, isSeller, isAdmin } = require('../middlewares/middleware');
 
 // cart masterlist
-router.get('/', Controller.showCart);
+router.get('/', isAdmin, Controller.showCart);
 
 // user's cart
-router.get('/:cartId', Controller.showCart);
+router.get('/:userId', isCustomer, Controller.showCart);
 
 // add cart
-router.get('/:productId/addToCart', Controller.addToCart)
+router.get('/:productId/addToCart', isCustomer, Controller.addToCart)
 
 // manipulate buying amount
-router.get('/:cartId/add', Controller.addAmount);
-router.get('/:cartId/sub', Controller.subtractAmount);
+router.get('/:productId/add', Controller.addAmount);
+router.get('/:productId/sub', Controller.subtractAmount);
 
 // confirm payment
-router.get('/:cartId/confirm', Controller.confirmPay);
+router.get('/:userId/confirm', isCustomer, Controller.confirmPay);
 
 // delete cart data
-router.get('/:cartId/delete', Controller.deleteCart);
+router.get('/:productId/delete', isCustomer, Controller.deleteCart);
 
 
 module.exports = router;
