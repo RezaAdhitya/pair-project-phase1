502b79489e04d9d84b7564a8f8a9c9fb6444c254
Reza Adhitya <rezaadhitya1995@gmail.com>
2023-01-12 10:48:41 +0700

    revisi router

--- app.js
+++ app.js
@@ -3,29 +3,12 @@ const router = require('./routes')
 const app = express()
 const port = 3000
 const router = require('./routers/index')
-const userRouter = require('./routers/users')
-const categoryRouter = require('./routers/categories')
-const productRouter = require('./routers/products')
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: true}))
 app.use(express.static(__dirname + '/public'));
 
-
-// landing page
-app.use('/', router)
-
-// product
-app.use('/products', productRouter)
-
-// categories
-app.use('/categories', categoryRouter)
-
-// users
-app.use('/users', userRouter)
-
 app.use(router)
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,27 @@
 const express = require('express')
 const router = express.Router()
 const categoryRoute = require('./category');
+const userRouter = require('./routers/users')
+const categoryRouter = require('./routers/categories')
+const productRouter = require('./routers/products')
 
 
 router.get('/', (req, res) => {
   res.render('index')
 })
+
+// landing page
+app.use('/', router)
+
+// product
+app.use('/products', productRouter)
+
+// categories
+app.use('/categories', categoryRouter)
+
+// users
+app.use('/users', userRouter)
+
 // define the about route
 router.use('/categories', categoryRoute)
 
